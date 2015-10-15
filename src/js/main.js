8d16fba0b1dfc944bede3530301c97a3508aedd6
deostroll <deostroll@gmail.com>
2015-10-16 01:15:18 +0530

    randomized colors

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@ function run(el) {
 	var center = { x: stage.getWidth()/2, y: stage.getHeight()/2 };
 	var layer = new Kinetic.Layer();
 	var params = {
-		sides: 5,
+		sides: 6,
 		radius: 50,
 		x: center.x,
 		y: center.y
@@ -57,7 +57,8 @@ function run(el) {
 			x: C.x,
 			y: C.y,
 			radius: params.dia/2,
-			fill: colors[i].toString()
+			fill: colors[i].toString(),
+			visible: true
 		});		
 
 		layer.add(circle);
@@ -90,40 +91,42 @@ function run(el) {
 
 function getRandomColors(n) {
 	var Color = net.brehaut.Color;
-	var red = Color("#ff0000");
+
+	var s = 0.5, l = 0.5;
+	var dx = Math.floor(360 / n);
+	var seed = Math.floor(Math.random() * 360);
 	var arr = [];
-	while(arr.length !== n) {
-		var angle = parseInt(Math.random() * 360);
-		if(arr.indexOf(angle) > -1) { continue; }
-		arr.push(angle);
+	for(var i = 0; i < n; i++, seed += dx) {
+		if(seed >= 360) {
+			seed = 0;			
+		}
+		var a = {
+			hue: seed,
+			saturation: s,
+			lightness: l
+		};
+		arr.push(Color(a).toString());
 	}
-	console.log(arr);
-	return arr.map(function(a) {
-		return red.shiftHue(a).darkenByRatio(0.1).desaturateByAmount(0.2).toString();
-	});
+
+	return arr;
 }
 
-// function hslToRgb(h, s, l) {
-//     var r, g, b;
 
-//     if (s == 0) {
-//         r = g = b = l; // achromatic
-//     } else {
-//         var hue2rgb = function hue2rgb(p, q, t) {
-//             if (t < 0) t += 1;
-//             if (t > 1) t -= 1;
-//             if (t < 1 / 6) return p + (q - p) * 6 * t;
-//             if (t < 1 / 2) return q;
-//             if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-//             return p;
-//         }
+// var TweenArray = function() {
 
-//         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-//         var p = 2 * l - q;
-//         r = hue2rgb(p, q, h + 1 / 3);
-//         g = hue2rgb(p, q, h);
-//         b = hue2rgb(p, q, h - 1 / 3);
-//     }
+// };
 
-//     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-// }
+// TweenArray.prototype = Array.prototype;
+
+// TweenArray.prototype.push = function() {
+// 	var args = this.slice.call(arguments);
+// 	var idx = this.length;
+// 	for(var i = 0, j = args.length; i < j; i++) {
+// 		this[idx] = args[i];
+// 		if(i === j - 1) {
+// 			args[i].onFinish = function() {
+
+// 			};
+// 		}
+// 	}
+// };
